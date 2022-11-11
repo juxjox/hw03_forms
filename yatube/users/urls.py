7ca1ba6613903b31b1bfcaf7cbb95a4c3f0b86cf
Полина Леуш <everfree@ya.ru>
2022-11-11 03:54:45 +0300

    Косметические правки PEP 8

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -23,35 +23,35 @@ urlpatterns = [
         name="logout",
     ),
     path(
-        "login/", 
-        LoginView.as_view(template_name="users/login.html"), 
+        "login/",
+        LoginView.as_view(template_name="users/login.html"),
         name="login"
-        ),
+    ),
     path(
         "password_change/",
         PasswordChangeView.as_view(
             template_name="users/password_change_form.html"
-            ),
+        ),
         name="password_change_form",
     ),
     path(
         "password_change/done/",
         PasswordChangeDoneView.as_view(
             template_name="users/password_change_done.html"
-            ),
+        ),
     ),
     path(
         "password_reset/",
         PasswordResetView.as_view(
             template_name="users/password_reset_form.html"
-            ),
+        ),
         name="password_reset_form",
     ),
     path(
         "password_reset/done/",
         PasswordResetDoneView.as_view(
             template_name="users/password_reset_done.html"
-            ),
+        ),
     ),
     path(
         "reset/<uidb64>/<token>/",
